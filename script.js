8dd4e904ec4a4c4391484df2c2c685bb37be5305
RusAlex91 <aocrusalex91@yandex.ru>
2021-09-09 15:55:32 +0300

    add test case 1

--- script.js
+++ script.js
@@ -248,6 +248,18 @@ const portfolioCases = {
       src: './images/work-sample/complex/tzar.gif',
       webLink: 'https://rusalex91.github.io/projects/heavy/tzar/'
     }
+  },
+  testing: {
+    1: {
+      id: 1,
+      name: 'Testing Case: Gallery',
+      desc:
+        'Gallery with drag and drop, JSON file/IMG file upload, URL upload, switch css for alternative gallery look and preloader.',
+      tech: 'HTML, CSS, JavaScript',
+      github: 'https://github.com/RusAlex91/frontend-test-assignment',
+      src: './images/work-sample/simple/test_case_1.gif',
+      webLink: 'https://rusalex91.github.io/projects/light/testing_case_1/'
+    }
   }
 }
 
